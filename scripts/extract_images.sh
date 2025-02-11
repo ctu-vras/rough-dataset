52e14771fdb36c3cf8aec3b5941c82c968c71504
Martin Pecka <peckama2@fel.cvut.cz>
2025-02-11 02:55:03 +0100

    Speed up extracted videos.

--- scripts/extract_images.sh
+++ scripts/extract_images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for f in *_20??-??-??-??-??-??.bag; do
-	rosrun cras_bag_tools extract_images -r -f mp4 "$f" "${f%.*}"
+	rosrun cras_bag_tools extract_images -r -f mp4 --speedup 10.0 --out-fps 25.0 "$f" "${f%.*}"
 done
