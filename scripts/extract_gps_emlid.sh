b8d2f398720b2690bd3bb8b1a3777c18ddf3f5fc
Martin Pecka <peckama2@fel.cvut.cz>
2025-10-20 14:44:34 +0200

    Separate emlid fix.

--- scripts/extract_gps_emlid.sh
+++ scripts/extract_gps_emlid.sh
@@ -2,10 +2,10 @@
 
 for f in *.no_sensors.bag; do
         grepros --topic /fix -d sensor_msgs/NavSatFix -ef latitude longitude altitude 'position_covariance' --write "fix.${f}.csv" overwrite=true --no-console-output -n "$f"
-        mv "fix.${f}."*".csv" "${f}.fix.csv"
-        sed -i 's/ /,/g' "${f}.fix.csv"
-        sed -i '1 s/^/#/' "${f}.fix.csv"
+        mv "fix.${f}."*".csv" "${f}.fix_emlid.csv"
+        sed -i 's/ /,/g' "${f}.fix_emlid.csv"
+        sed -i '1 s/^/#/' "${f}.fix_emlid.csv"
         gpsbabel -t -i unicsv,fields=comment+date+time+comment+comment+lat+lon+alt+hdop+comment+comment+comment+comment+comment+comment+comment+vdop -f "${f}.fix.csv" -x simplify,relative,error=1 -o gpx -F "${f}
-.fix.gpx"
+.fix_emlid.gpx"
         echo "Processed $f"
 done
