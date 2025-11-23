369f6298e87a503949f0c4efd02432647b0079fb
Martin Pecka <peckama2@fel.cvut.cz>
2025-11-23 20:53:19 +0100

    Updated preproc scripts, added visualization tools configs.

---
+++ scripts/extract_gps_tatra.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for f in *.no_sensors.bag; do
+        grepros --topic /fix -d sensor_msgs/NavSatFix -ef latitude longitude altitude 'position_covariance' --write "fix.${f}.csv" overwrite=true --no-console-output -n "$f"
+        mv "fix.${f}."*".csv" "${f}.fix_tatra.csv"
+        sed -i 's/ /,/g' "${f}.fix_tatra.csv"
+        sed -i '1 s/^/#/' "${f}.fix_tatra.csv"
+        gpsbabel -t -i unicsv,fields=comment+date+time+comment+comment+lat+lon+alt+hdop+comment+comment+comment+comment+comment+comment+comment+vdop -f "${f}.fix_tatra.csv" -x simplify,crosstrack,error=1m -o gpx -F "${f}.fix_tatra.gpx"
+        echo "Processed $f"
+done
