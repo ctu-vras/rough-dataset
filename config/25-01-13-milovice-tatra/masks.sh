f00b41990469ce8c8645770ca0ca1bf9d48fb518
Martin Pecka <peckama2@fel.cvut.cz>
2025-11-25 03:26:16 +0100

    Added camera mask processing to tatra.

---
+++ config/25-01-13-milovice-tatra/masks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+[[ "$(pwd)" == *25-01-13-milovice-tatra ]] || (echo "This script has to be run in folder 25-01-13-milovice-tatra !" 1>&2; exit 1) 
+
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-tatra.yaml" -- tatra_2025-01-13-14-11-33.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-tatra.yaml" -- tatra_2025-01-13-14-17-04.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-tatra.yaml" -- tatra_2025-01-13-16-25-31.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-tatra.yaml" -- tatra_2025-01-13-16-35-07.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-tatra.yaml" -- tatra_2025-01-13-16-43-29.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-tatra.yaml" -- tatra_2025-01-13-16-53-30.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-tatra.yaml" -- tatra_2025-01-13-16-57-32.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-tatra.yaml" -- tatra_2025-01-13-17-00-46.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-tatra.yaml" -- tatra_2025-01-13-17-27-32.preproc.bag
