0d30e4c9a5e493066bb40aefd689c1cfc5c47cf6
Martin Pecka <peckama2@fel.cvut.cz>
2025-11-26 16:57:04 +0100

    Added all configs for damaged images.

---
+++ config/24-09-26-petrin-marv_clone/damaged-images.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+[[ "$(pwd)" == *24-09-26-petrin-marv_clone ]] || (echo "This script has to be run in folder 24-09-26-petrin-marv_clone !" 1>&2; exit 1) 
+
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/damaged-images-clone.yaml" -- \
+  ugv_2024-09-26-13-54-18.bag \
+  ugv_2024-09-26-13-58-46.bag \
+  ugv_2024-09-26-14-03-57.bag \
+	ugv_2024-09-26-14-14-42.bag
+
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/damaged-images-marv.yaml" -- \
+  marv_2024-09-26-13-46-51.bag \
+	marv_2024-09-26-13-54-43.bag
+	
---
+++ config/24-11-25-cimicky_haj/damaged-images.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+[[ "$(pwd)" == *24-11-25-cimicky_haj ]] || (echo "This script has to be run in folder 24-11-25-cimicky_haj !" 1>&2; exit 1) 
+
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/damaged-images-marv.yaml" -- \
+	marv_2024-11-25-02-21-37.bag \
+	marv_2024-11-25-02-38-54.bag \
+	marv_2024-11-25-02-40-43.bag \
+	marv_2024-11-25-02-44-14.bag \
+	marv_2024-11-25-02-50-38.bag \
+	marv_2024-11-25-03-06-00.bag \
+	marv_2024-11-25-03-09-36.bag \
+	marv_2024-11-25-03-17-48.bag \
+	marv_2024-11-25-03-32-49.bag \
+	marv_2024-11-25-03-38-06.bag \
+	marv_2024-11-25-03-46-38.bag \
+	marv_2024-11-25-03-54-33.bag \
+	marv_2024-11-25-04-03-37.bag
