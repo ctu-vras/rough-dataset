c61a0ce4f2ddccd4769db482ed4bbdbc87d27e6f
Martin Pecka <peckama2@fel.cvut.cz>
2025-11-26 11:23:37 +0100

    Added detection of damaged images.

--- config/25-01-13-milovice-tatra/damaged-images.sh
+++ config/25-01-13-milovice-tatra/damaged-images.sh
@@ -4,12 +4,13 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 [[ "$(pwd)" == *25-01-13-milovice-tatra ]] || (echo "This script has to be run in folder 25-01-13-milovice-tatra !" 1>&2; exit 1) 
 
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/damaged-images-tatra.yaml" -- tatra_2025-01-13-14-11-33.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/damaged-images-tatra.yaml" -- tatra_2025-01-13-14-17-04.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/damaged-images-tatra.yaml" -- tatra_2025-01-13-16-25-31.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/damaged-images-tatra.yaml" -- tatra_2025-01-13-16-35-07.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/damaged-images-tatra.yaml" -- tatra_2025-01-13-16-43-29.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/damaged-images-tatra.yaml" -- tatra_2025-01-13-16-53-30.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/damaged-images-tatra.yaml" -- tatra_2025-01-13-16-57-32.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/damaged-images-tatra.yaml" -- tatra_2025-01-13-17-00-46.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/damaged-images-tatra.yaml" -- tatra_2025-01-13-17-27-32.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/damaged-images-tatra.yaml" -- \
+  tatra_2025-01-13-14-11-33.bag \
+  tatra_2025-01-13-14-17-04.bag \
+  tatra_2025-01-13-16-25-31.bag \
+  tatra_2025-01-13-16-35-07.bag \
+  tatra_2025-01-13-16-43-29.bag \
+  tatra_2025-01-13-16-53-30.bag \
+  tatra_2025-01-13-16-57-32.bag \
+  tatra_2025-01-13-17-00-46.bag \
+  tatra_2025-01-13-17-27-32.bag
