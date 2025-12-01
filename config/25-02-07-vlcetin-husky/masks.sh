2c46d89fd445a1e8160f4ad4646d1c68c3f52d60
Martin Pecka <peckama2@fel.cvut.cz>
2025-12-01 11:45:01 +0100

    More info about 25-03-19-petrin.

--- config/25-02-07-vlcetin-husky/masks.sh
+++ config/25-02-07-vlcetin-husky/masks.sh
@@ -4,15 +4,15 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 [[ "$(pwd)" == *25-02-07-vlcetin-husky ]] || (echo "This script has to be run in folder 25-02-07-vlcetin-husky !" 1>&2; exit 1) 
 
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky_2025-02-07-15-10-53.yaml" -- husky_2025-02-07-15-10-53.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky_2025-02-07-15-17-04.yaml" -- husky_2025-02-07-15-17-04.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky_2025-02-07-15-18-09.yaml" -- husky_2025-02-07-15-18-09.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky_2025-02-07-15-27-03.yaml" -- husky_2025-02-07-15-27-03.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky.yaml" -- husky_2025-02-07-16-06-49.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky.yaml" -- husky_2025-02-07-16-16-56.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky.yaml" -- husky_2025-02-07-16-20-58.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky.yaml" -- husky_2025-02-07-16-22-48.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky.yaml" -- husky_2025-02-07-16-35-45.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky_2025-02-07-16-46-52.yaml" -- husky_2025-02-07-16-46-52.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky_2025-02-07-16-55-40.yaml" -- husky_2025-02-07-16-55-40.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky.yaml" -- husky_2025-02-07-17-18-26.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky_2025-02-07-15-10-53.yaml" -- husky_2025-02-07-15-10-53.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky_2025-02-07-15-17-04.yaml" -- husky_2025-02-07-15-17-04.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky_2025-02-07-15-18-09.yaml" -- husky_2025-02-07-15-18-09.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky_2025-02-07-15-27-03.yaml" -- husky_2025-02-07-15-27-03.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky.yaml" -- husky_2025-02-07-16-06-49.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky.yaml" -- husky_2025-02-07-16-16-56.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky.yaml" -- husky_2025-02-07-16-20-58.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky.yaml" -- husky_2025-02-07-16-22-48.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky.yaml" -- husky_2025-02-07-16-35-45.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky_2025-02-07-16-46-52.yaml" -- husky_2025-02-07-16-46-52.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky_2025-02-07-16-55-40.yaml" -- husky_2025-02-07-16-55-40.preproc.bag
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-husky.yaml" -- husky_2025-02-07-17-18-26.preproc.bag
---
+++ config/25-03-19-petrin/masks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+[[ "$(pwd)" == *25-03-19-petrin ]] || (echo "This script has to be run in folder 25-03-19-petrin !" 1>&2; exit 1) 
+
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/masks-marv.yaml" -- \
+	marv_2025-03-19-14-41-19.preproc.bag \
+  marv_2025-03-19-14-45-10.preproc.bag \
+	marv_2025-03-19-14-47-44.preproc.bag \
+	marv_2025-03-19-15-03-51.preproc.bag \
+	marv_2025-03-19-15-22-35.preproc.bag \
+	marv_2025-03-19-15-24-35.preproc.bag \
+	marv_2025-03-19-15-33-00.preproc.bag \
+	marv_2025-03-19-15-36-49.preproc.bag
+	
