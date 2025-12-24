34d26987e20d364fc487fe9c3438aa5bd1dac4be
Martin Pecka <peckama2@fel.cvut.cz>
2025-12-25 07:28:25 +0800

    Added image and pcd extraction scripts.

---
+++ config/24-09-10-clone-petrin/extract_pcd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+[[ "$(pwd)" == *24-09-10-clone-petrin ]] || (echo "This script has to be run in folder 24-09-10-clone-petrin !" 1>&2; exit 1) 
+
+for f in \
+  ugv_2024-09-10-17-02-31.postproc.bag \
+  ugv_2024-09-10-17-12-12.postproc.bag \
+  ; do
+    rosrun rosrun cras_bag_tools extract_pcd -v `pwd`/$f ${f%.postproc.bag} --zip -c 6
+done
