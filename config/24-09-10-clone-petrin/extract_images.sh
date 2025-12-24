b0f62fc6edc1901ff54d490d9204a94ca7ec54a2
Martin Pecka <peckama2@fel.cvut.cz>
2025-12-25 07:36:37 +0800

    Simplified

--- config/24-09-10-clone-petrin/extract_images.sh
+++ config/24-09-10-clone-petrin/extract_images.sh
@@ -4,9 +4,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 [[ "$(pwd)" == *24-09-10-clone-petrin ]] || (echo "This script has to be run in folder 24-09-10-clone-petrin !" 1>&2; exit 1) 
 
-for f in \
-  ugv_2024-09-10-17-02-31.postproc.bag \
-  ugv_2024-09-10-17-12-12.postproc.bag \
-  ; do
+for f in *_????-??-??-??-??-??.postproc.bag; do
     rosrun cras_bag_tools extract_images -v `pwd`/$f ${f%.postproc.bag} --zip -c 1 --image-name-pattern '{topic}.{secs}.{nsecs:09d}.{format}'
 done
