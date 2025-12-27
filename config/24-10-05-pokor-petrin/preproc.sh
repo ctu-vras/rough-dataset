b6a547cf45088ac8a44f90e69986eeb58ffe072c
Martin Pecka <peckama2@fel.cvut.cz>
2025-12-27 12:02:59 +0800

    added preproc for 24-10-05

---
+++ config/24-10-05-pokor-petrin/preproc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+[[ "$(pwd)" == *24-10-05-pokor-petrin ]] || { echo "This script has to be run in folder 24-10-05-pokor-petrin !" 1>&2; exit 1; } 
+
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/preproc-ugv.yaml" -- \
+  ugv_2024-10-05-15-40-41.bag \
+  ugv_2024-10-05-15-48-31.bag \
+  ugv_2024-10-05-15-58-52.bag \
+  ugv_2024-10-05-16-08-30.bag \
+	ugv_2024-10-05-16-24-48.bag
+
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/preproc-marv1.yaml" -- \
+  marv_2024-10-05-12-34-53.bag \
+  marv_2024-10-05-13-01-40.bag
+
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/preproc-marv2.yaml" -- \
+  marv_2024-10-05-13-17-08.bag \
+  marv_2024-10-05-13-29-39.bag \
+  marv_2024-10-05-13-43-21.bag \
+  marv_2024-10-05-13-57-57.bag \
+  marv_2024-10-05-14-12-29.bag \
+  marv_2024-10-05-14-22-10.bag
+
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/preproc-marv3.yaml" -- \
+  marv_2024-10-05-14-28-15.bag
+	
