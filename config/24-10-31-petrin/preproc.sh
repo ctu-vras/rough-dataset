c2120203886b8cc133bb24095c19c9ed2f280811
Martin Pecka <peckama2@fel.cvut.cz>
2025-12-27 16:19:50 +0800

    fix

--- config/24-10-31-petrin/preproc.sh
+++ config/24-10-31-petrin/preproc.sh
@@ -8,5 +8,7 @@ rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/preproc-marv.yaml" -- \
   marv_2024-10-31-15-16-42.bag \
   marv_2024-10-31-15-26-47.bag \
   marv_2024-10-31-15-35-05.bag \
-  marv_2024-10-31-15-52-07.bag \
   marv_2024-10-31-15-56-33.bag
+
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/preproc-marv.yaml" --end-time 109 -- \
+  marv_2024-10-31-15-52-07.bag
