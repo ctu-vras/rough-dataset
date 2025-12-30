fabf3f767e026139a72495fcdd05f9b1fba52244
Martin Pecka <peckama2@fel.cvut.cz>
2025-12-31 00:56:59 +0800

    Better preproc of missing static tfs in 24-11-25

--- config/24-11-25-cimicky_haj/create_static_tf_bag.sh
+++ config/24-11-25-cimicky_haj/create_static_tf_bag.sh
@@ -4,10 +4,10 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 [[ "$(pwd)" == *24-11-25-cimicky_haj ]] || { echo "This script has to be run in folder 24-11-25-cimicky_haj !" 1>&2; exit 1; } 
 
-good_bag="marv_2024-11-25-03-17-48"
+good_bag="marv_2024-11-25-04-03-37"
 good_bag_start=$(rosbag info -k start -y "${good_bag}.bag")
 
-bad_bags=("marv_2024-11-25-02-21-37" "marv_2024-11-25-02-38-54" "marv_2024-11-25-02-44-14" "marv_2024-11-25-02-50-38" "marv_2024-11-25-03-06-00" "marv_2024-11-25-03-09-36" "marv_2024-11-25-03-38-06" "marv_2024-11-25-03-54-33")
+bad_bags=("marv_2024-11-25-02-21-37" "marv_2024-11-25-02-38-54" "marv_2024-11-25-02-40-43" "marv_2024-11-25-02-44-14" "marv_2024-11-25-02-50-38" "marv_2024-11-25-03-06-00" "marv_2024-11-25-03-09-36" "marv_2024-11-25-03-17-48" "marv_2024-11-25-03-32-49" "marv_2024-11-25-03-38-06" "marv_2024-11-25-03-46-38" "marv_2024-11-25-03-54-33")
 
 for bag in "${bad_bags[@]}"; do
   echo "Processing ${bag}.bag"
--- config/24-11-25-cimicky_haj/preproc.sh
+++ config/24-11-25-cimicky_haj/preproc.sh
@@ -5,6 +5,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 [[ "$(pwd)" == *24-11-25-cimicky_haj ]] || { echo "This script has to be run in folder 24-11-25-cimicky_haj !" 1>&2; exit 1; } 
 
 rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/preproc-marv.yaml" -- \
+  marv_2024-11-25-04-03-37.bag
+
+"${SCRIPT_DIR}"/create_static_tf_bag.sh
+
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/preproc-marv.yaml" -- \
 	marv_2024-11-25-02-21-37.bag \
 	marv_2024-11-25-02-38-54.bag \
 	marv_2024-11-25-02-40-43.bag \
@@ -16,5 +21,4 @@ rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/preproc-marv.yaml" -- \
 	marv_2024-11-25-03-32-49.bag \
 	marv_2024-11-25-03-38-06.bag \
 	marv_2024-11-25-03-46-38.bag \
-	marv_2024-11-25-03-54-33.bag \
-	marv_2024-11-25-04-03-37.bag
+	marv_2024-11-25-03-54-33.bag
