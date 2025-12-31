7a6f6effc4d5df8988624015289b233ccf231d29
Martin Pecka <peckama2@fel.cvut.cz>
2025-12-31 08:05:18 +0800

    fix

--- config/24-10-05-pokor-petrin/postproc.sh
+++ config/24-10-05-pokor-petrin/postproc.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-[[ "$(pwd)" == *24-10-05-pokor-petrin ]] || { echo "This script has to be run in folder 24-10-05-pokor-petrin !" 1>&2; exit 1; }[[ "$(pwd)" == *24-10-05-pokor-petrin ]] || { echo "This script has to be run in folder 24-10-05-pokor-petrin !" 1>&2; exit 1; }
+[[ "$(pwd)" == *24-10-05-pokor-petrin ]] || { echo "This script has to be run in folder 24-10-05-pokor-petrin !" 1>&2; exit 1; }
 
 src_dir=
 if [[ $# -gt 0 ]]; then
