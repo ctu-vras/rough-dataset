d60c06b94e954abf7752964c5c83f5e2ebb4b91d
Martin Pecka <peckama2@fel.cvut.cz>
2026-01-01 22:15:46 +0800

    Added extract images and pcd to tatra and husky

---
+++ config/25-01-13-milovice-tatra/extract_pcd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+[[ "$(pwd)" == *25-01-13-milovice-tatra ]] || { echo "This script has to be run in folder 25-01-13-milovice-tatra !" 1>&2; exit 1; } 
+
+for f in *_????-??-??-??-??-??.postproc.bag; do
+    rosrun cras_bag_tools extract_pcd -v `pwd`/$f ${f%.postproc.bag} --zip -c 6
+done
