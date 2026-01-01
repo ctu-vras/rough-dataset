7c3491cec5c433e9303c8589217cf3162a25bd4a
Martin Pecka <peckama2@fel.cvut.cz>
2026-01-02 00:49:20 +0800

    Added extract images and pcd to 24-10-05

---
+++ config/24-10-05-pokor-petrin/extract_images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+[[ "$(pwd)" == *24-10-05-pokor-petrin ]] || { echo "This script has to be run in folder 24-10-05-pokor-petrin !" 1>&2; exit 1; } 
+
+for f in *_????-??-??-??-??-??.postproc.bag; do
+    rosrun cras_bag_tools extract_images -v `pwd`/$f ${f%.postproc.bag} --zip -c 1 --image-name-pattern '{topic}.{secs}.{nsecs:09d}.{format}'
+done
