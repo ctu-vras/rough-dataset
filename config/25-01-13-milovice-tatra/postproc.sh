aad2c2a0a00f09e2de7d5a70bee164bcc29f774f
Martin Pecka <peckama2@fel.cvut.cz>
2026-01-05 14:22:42 +0100

    Fixed postproc for tatra

--- config/25-01-13-milovice-tatra/postproc.sh
+++ config/25-01-13-milovice-tatra/postproc.sh
@@ -4,12 +4,20 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 [[ "$(pwd)" == *25-01-13-milovice-tatra ]] || { echo "This script has to be run in folder 25-01-13-milovice-tatra !" 1>&2; exit 1; } 
 
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/postproc-tatra.yaml" --lz4 tatra_2025-01-13-14-11-33.preproc.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/postproc-tatra.yaml" --lz4 tatra_2025-01-13-14-17-04.preproc.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/postproc-tatra.yaml" --lz4 tatra_2025-01-13-16-25-31.preproc.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/postproc-tatra.yaml" --lz4 tatra_2025-01-13-16-35-07.preproc.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/postproc-tatra.yaml" --lz4 tatra_2025-01-13-16-43-29.preproc.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/postproc-tatra.yaml" --lz4 tatra_2025-01-13-16-53-30.preproc.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/postproc-tatra.yaml" --lz4 tatra_2025-01-13-16-57-32.preproc.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/postproc-tatra.yaml" --lz4 tatra_2025-01-13-17-00-46.preproc.bag
-rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/postproc-tatra.yaml" --lz4 tatra_2025-01-13-17-27-32.preproc.bag
+src_dir=
+if [[ $# -gt 0 ]]; then
+	src_dir="${1}/"
+fi
+
+echo $src_dir
+
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/postproc-tatra.yaml" -- \
+	"${src_dir}/tatra_2025-01-13-14-11-33.preproc.bag" \
+	"${src_dir}/tatra_2025-01-13-14-17-04.preproc.bag" \
+	"${src_dir}/tatra_2025-01-13-16-25-31.preproc.bag" \
+	"${src_dir}/tatra_2025-01-13-16-35-07.preproc.bag" \
+	"${src_dir}/tatra_2025-01-13-16-43-29.preproc.bag" \
+	"${src_dir}/tatra_2025-01-13-16-53-30.preproc.bag" \
+	"${src_dir}/tatra_2025-01-13-16-57-32.preproc.bag" \
+	"${src_dir}/tatra_2025-01-13-17-00-46.preproc.bag" \
+	"${src_dir}/tatra_2025-01-13-17-27-32.preproc.bag"
