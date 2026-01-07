a4fa6f4d657cef3ddecaa292ba8498f0717a367e
Martin Pecka <peckama2@fel.cvut.cz>
2026-01-07 11:05:11 +0100

    Better naming for preview videos.

--- scripts/extract_images.sh
+++ scripts/extract_images.sh
@@ -4,5 +4,5 @@ for f in *_20??-??-??-??-??-??.bag *_20??-??-??-??-??-??.postproc.bag; do
 	[ -f "$f" ] || continue
 	dir="${f%.postproc.bag}"
 	dir="${dir%.bag}"
-	rosrun cras_bag_tools extract_images -r -f mp4 --speedup 10.0 --out-fps 25.0 "$f" "$dir"
+	rosrun cras_bag_tools extract_images -r -f mp4 --speedup 10.0 --out-fps 25.0 --video-name-pattern 'preview_{topic}.{format}' "$f" "$dir"
 done
