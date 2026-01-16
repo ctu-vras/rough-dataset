378f2f730156aba091dcf065146f58786cba4e0c
Martin Pecka <peckama2@fel.cvut.cz>
2026-01-16 20:54:46 +0100

    Extract CSVs from all datasets.

---
+++ config/25-01-13-milovice-tatra/extract_csvs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+dataset="$(basename "$SCRIPT_DIR")"
+[[ "$(pwd)" == *"$dataset" ]] || { echo "This script has to be run in folder ${dataset} !" 1>&2; exit 1; } 
+
+rosrun cras_bag_tools filter_bag -c "${SCRIPT_DIR}/../extract_csvs.yaml" -- *_????-??-??-??-??-??.postproc.no_sensors.bag
