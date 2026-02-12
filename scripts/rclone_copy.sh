f56a324c94d13a9328a4021e8fc76b98ceb7b9ce
Martin Pecka <peckama2@fel.cvut.cz>
2026-02-12 23:53:56 +0100

    Update rclone.

--- scripts/rclone_copy.sh
+++ scripts/rclone_copy.sh
@@ -5,12 +5,12 @@
 rclone copy -P \
   --filter '- Ogre.log' --filter '- photodoc/**' --filter '- /*/*_????-??-??-??-??-??.no_sensors.bag.params' --filter '- tmp/**' --filter '- /*/*preproc*' \
   --filter '+ /*/*.postproc.*' --filter '+ /*/*/*compressed.zip' --filter '+ /*/*/*compressedDepth.zip' --filter '+ /*/*/points_filtered.zip' \
-  --filter '+ /*/*/preview_*.mp4' --filter '+ /*/*.{csv,gpx,log,params,sdf,srt,urdf}' \
-  --filter '+ /*/*.caminfo.*.yaml' --filter '+ /*/*.calib.yaml' --filter '+ /*/*.transformations.yaml' \
+  --filter '+ /*/*/preview_*.mp4' --filter '+ /*/*.{csv,gpx,log,params,sdf,srt,urdf}' --filter '+ /*/*/*.csv' \
+  --filter '+ /*/*/*.caminfo.*.yaml' --filter '+ /*/*/*.calib.yaml' --filter '+ /*/*/*.transformations.yaml' \
   --filter '+ /*/*.mask.pbm' --filter '+ /*/*.mask.png' \
   --filter '+ /*/*/resized/*.zip' --filter '+ /*/*/wildscenes_seg/*.zip' --filter '+ /*/*/wildscenes_vis/*.zip'  \
   --filter '+ /*/*.external_cams_blurred.bag' --filter '+ /*/*_gnss.zip' \
   --filter '+ /calib/**' \
   --filter '- **' \
-  --drive-chunk-size 512M --fast-list --transfers=6 --checkers=6 --buffer-size=75M \
+  --drive-chunk-size 512M --fast-list --transfers=6 --checkers=6 --buffer-size=75M -L \
   . gdrive:ROUGH/
