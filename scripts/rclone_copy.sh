f67a38849d0d137ebfbebcc38ce11c0567befa03
Martin Pecka <peckama2@fel.cvut.cz>
2026-03-10 18:07:21 +0100

    Extended copy command.

--- scripts/rclone_copy.sh
+++ scripts/rclone_copy.sh
@@ -6,7 +6,7 @@ rclone copy -P \
   --filter '- Ogre.log' --filter '- photodoc/**' --filter '- /*/*_????-??-??-??-??-??.no_sensors.bag.params' --filter '- tmp/**' --filter '- /*/*preproc*' \
   --filter '+ /*/*.postproc.*' --filter '+ /*/*/*compressed.zip' --filter '+ /*/*/*compressedDepth.zip' --filter '+ /*/*/points_filtered.zip' \
   --filter '+ /*/*/preview_*.mp4' --filter '+ /*/*.{csv,gpx,log,md,params,sdf,srt,urdf}' --filter '+ /*/*/*.{csv,length}' \
-  --filter '+ /*/*/*.caminfo.*.yaml' --filter '+ /*/*/*.calib.yaml' --filter '+ /*/*/*.transformations.yaml' \
+  --filter '+ /*/*/*caminfo.*.yaml' --filter '+ /*/*/*calib.yaml' --filter '+ /*/*/*transformations.yaml' --filter '+ /*/*/weak_unsafe.csv' \
   --filter '+ /*/*.mask.pbm' --filter '+ /*/*.mask.png' \
   --filter '+ /README.md' \
   --filter '+ /*/*/resized/*.zip' --filter '+ /*/*/wildscenes_seg/*.zip' --filter '+ /*/*/wildscenes_vis/*.zip'  \
